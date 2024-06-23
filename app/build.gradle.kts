62a40046857f777c86ad26307f02df13d0b8595a
joonbo97 <joonbo97@naver.com>
2024-06-23 18:36:28 +0900

    sdk change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.store_me.storeme"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.store_me.storeme"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
