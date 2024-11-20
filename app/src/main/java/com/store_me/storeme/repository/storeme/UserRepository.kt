97bc6d93e79ffac8d21a0256147504967e0d4877
joonbo97 <joonbo97@naver.com>
2024-11-21 00:49:00 +0900

    feat : 에러 핸들링 추가

--- app/src/main/java/com/store_me/storeme/repository/storeme/UserRepository.kt
+++ app/src/main/java/com/store_me/storeme/repository/storeme/UserRepository.kt
@@ -1,5 +1,7 @@
 package com.store_me.storeme.repository.storeme
 
+import androidx.compose.ui.res.stringResource
+import com.store_me.storeme.R
 import com.store_me.storeme.data.model.StoreMeResponse
 import com.store_me.storeme.network.storeme.UserApiService
 import javax.inject.Inject
@@ -24,10 +26,10 @@ class UserRepositoryImpl @Inject constructor(
                         Result.success(Unit)
                     }
                     false -> {
-                        Result.failure(Exception(response.message() ?: "오류가 발생했습니다. 다시 시도해주세요."))
+                        Result.failure(Exception(response.message()))
                     }
                     else -> {
-                        Result.failure(Exception(response.message() ?: "오류가 발생했습니다. 다시 시도해주세요."))
+                        Result.failure(Exception(response.message()))
                     }
                 }
             } else {
@@ -36,36 +38,6 @@ class UserRepositoryImpl @Inject constructor(
         } catch (e: Exception) {
             Result.failure(e)
         }
-
-        /*return try {
-            val response = locationApiService.getLocationList()
-            if(response.isSuccessful) {
-                //API 호출 성공
-                val data = response.body()
-
-                if (data != null) {
-                    //데이터 있는 경우
-                    Result.success(data)
-                } else {
-                    //데이터 없는 경우
-                    val errorBody = response.errorBody()?.string()
-
-                    val errorResponse = ErrorParser.setErrorResponse(errorBody)
-
-                    Result.failure(ApiException(errorResponse = errorResponse, ApiNames.LOCATIONS.apiName))
-                }
-            } else {
-                //API 호출 실패
-                val errorBody = response.errorBody()?.string()
-
-                val errorResponse = ErrorParser.setErrorResponse(errorBody)
-
-                Result.failure(ApiException(errorResponse = errorResponse, ApiNames.LOCATIONS.apiName))
-            }
-        } catch (e: Exception) {
-            //네트워크 오류 혹은 예외 발생
-            Result.failure(ApiException(errorResponse = ErrorResponse(null, null), ApiNames.LOCATIONS.apiName))
-        }*/
     }
 
 }
