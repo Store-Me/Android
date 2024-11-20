d4a9688f077e2a565623aef4af815b357cbc13d6
joonbo97 <joonbo97@naver.com>
2024-11-20 23:31:10 +0900

    feat: 에러 핸들링 추가

--- app/src/main/java/com/store_me/storeme/repository/storeme/UserRepository.kt
+++ app/src/main/java/com/store_me/storeme/repository/storeme/UserRepository.kt
@@ -8,13 +8,35 @@ import javax.inject.Inject
  * 사용자 관련 UserRepository
  */
 interface UserRepository {
-    suspend fun signupApp(): Result<StoreMeResponse<Unit>>
+    suspend fun signupApp(): Result<Unit>
 }
 
 class UserRepositoryImpl @Inject constructor(
     private val userApiService: UserApiService
 ): UserRepository {
-    override suspend fun signupApp(): Result<StoreMeResponse<Unit>> {
+    override suspend fun signupApp(): Result<Unit> {
+        return try {
+            val response = userApiService.signupApp()
+
+            if(response.isSuccessful) {
+                when(response.body()?.isSuccess) {
+                    true -> {
+                        Result.success(Unit)
+                    }
+                    false -> {
+                        Result.failure(Exception(response.message() ?: "오류가 발생했습니다. 다시 시도해주세요."))
+                    }
+                    else -> {
+                        Result.failure(Exception(response.message() ?: "오류가 발생했습니다. 다시 시도해주세요."))
+                    }
+                }
+            } else {
+                Result.failure(Exception("오류가 발생했습니다. ${response.code()} - ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+
         /*return try {
             val response = locationApiService.getLocationList()
             if(response.isSuccessful) {
