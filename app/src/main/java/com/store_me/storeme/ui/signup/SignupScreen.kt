94b2310b53ba1d78137e37ea0e38fc8970b202d6
joonbo97 <joonbo97@naver.com>
2024-11-20 23:30:53 +0900

    chore : 온보딩 화면 추가

---
+++ app/src/main/java/com/store_me/storeme/ui/signup/SignupScreen.kt
@@ -0,0 +1,9 @@
+package com.store_me.storeme.ui.signup
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SignupScreen(navController: NavController) {
+
+}
