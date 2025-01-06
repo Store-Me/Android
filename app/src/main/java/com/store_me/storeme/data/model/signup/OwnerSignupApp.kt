95bbb3119b68ad40e5d335581eaaa8bdc469f6c8
joonbo97 <joonbo97@naver.com>
2025-01-06 11:33:10 +0900

    feat : 사장님 가입 화면 추가

---
+++ app/src/main/java/com/store_me/storeme/data/model/signup/OwnerSignupApp.kt
@@ -0,0 +1,22 @@
+package com.store_me.storeme.data.model.signup
+
+data class OwnerSignupApp(
+    val accountId: String,
+    val password: String,
+    val phoneNumber: String,
+    val privacyConsent: Boolean,
+    val marketingConsent: Boolean,
+    val verificationCode: String,
+
+    val storeName: String,
+    val storeDescription: String?,
+    val storeCategory: String,
+    val storeDetailCategory: String?,
+    val storeLocation: String,
+    val storeLocationCode: Long,
+    val storeLocationDetail: String?,
+    val storeLat: Double?,
+    val storeLng: Double?,
+    val storePhoneNumber: String?,
+    val storeIntro: String?
+)
