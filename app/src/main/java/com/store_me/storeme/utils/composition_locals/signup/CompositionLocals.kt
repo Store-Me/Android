916f754b8187a390718160ad1de5eb2f03aff54b
joonbo97 <joonbo97@naver.com>
2025-01-06 11:06:41 +0900

    refactor : 가입 화면 CompositionLocal 분리

---
+++ app/src/main/java/com/store_me/storeme/utils/composition_locals/signup/CompositionLocals.kt
@@ -0,0 +1,44 @@
+package com.store_me.storeme.utils.composition_locals.signup
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.store_me.storeme.ui.signup.SignupViewModel
+import com.store_me.storeme.ui.signup.account_data.AccountDataViewModel
+import com.store_me.storeme.ui.signup.customer.CustomerDataViewModel
+import com.store_me.storeme.ui.signup.onboarding.SignupOnboardingViewModel
+import com.store_me.storeme.ui.signup.owner.StoreDataViewModel
+import com.store_me.storeme.ui.signup.phone_authentication.PhoneNumberViewModel
+import com.store_me.storeme.ui.signup.terms.TermsViewModel
+
+val LocalSnackbarHostState = compositionLocalOf<SnackbarHostState> {
+    error("No SnackbarHostState provided")
+}
+
+val LocalSignupViewModel = staticCompositionLocalOf<SignupViewModel> {
+    error("No LocalSignupViewModel provided")
+}
+
+val LocalTermsViewModel = staticCompositionLocalOf<TermsViewModel> {
+    error("No LocalTermsVieWModel Provided")
+}
+
+val LocalPhoneNumberViewModel = staticCompositionLocalOf<PhoneNumberViewModel> {
+    error("No PhoneNumberViewModel Provided")
+}
+
+val LocalAccountDataViewModel = staticCompositionLocalOf<AccountDataViewModel> {
+    error("No AccountDataViewModel Provided")
+}
+
+val LocalStoreDataViewModel = staticCompositionLocalOf<StoreDataViewModel> {
+    error("No StoreDataViewModel Provided")
+}
+
+val LocalSignupOnboardingViewModel = staticCompositionLocalOf<SignupOnboardingViewModel> {
+    error("No SignupOnboardingViewModel Provided")
+}
+
+val LocalCustomerDataViewModel = staticCompositionLocalOf<CustomerDataViewModel> {
+    error("No CustomerDataViewModel Provided")
+}
