290bed00094d5ce7ae4819bb034e7616f8ea8e48
joonbo97 <joonbo97@naver.com>
2025-01-07 16:16:50 +0900

    feat : SignupViewModel 내부 진행 관련 코드 SignupProgressViewModel로 분리

---
+++ app/src/main/java/com/store_me/storeme/ui/signup/signup_progress/CustomerProgress.kt
@@ -0,0 +1,7 @@
+package com.store_me.storeme.ui.signup.signup_progress
+
+enum class CustomerProgress {
+    NICKNAME,
+    PROFILE_IMAGE,
+    FINISH
+}
---
+++ app/src/main/java/com/store_me/storeme/ui/signup/signup_progress/OwnerProgress.kt
@@ -0,0 +1,13 @@
+package com.store_me.storeme.ui.signup.signup_progress
+
+enum class OwnerProgress {
+    STORE_NAME,
+    CATEGORY,
+    CUSTOM_CATEGORY,
+    ADDRESS,
+    STORE_PROFILE_IMAGE,
+    STORE_IMAGE,
+    INTRO,
+    NUMBER,
+    FINISH
+}
