55b2862283c9b15b55b29ce7723261888a8deac9
joonbo97 <joonbo97@naver.com>
2025-01-11 12:54:36 +0900

    feat : lottie 추가

--- app/src/main/java/com/store_me/storeme/ui/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/store_me/storeme/ui/onboarding/OnboardingScreen.kt
@@ -11,17 +11,36 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieAnimatable
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.store_me.storeme.R
 import com.store_me.storeme.ui.component.DefaultButton
 
 @Composable
 fun OnboardingScreen(navController: NavController) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.onboarding_animation)
+    )
+    val lottieAnimatable = rememberLottieAnimatable()
+
+    LaunchedEffect(composition) {
+        lottieAnimatable.animate(
+            composition = composition
+        )
+    }
+
     Scaffold(
         containerColor = Color.White,
         content = { innerPadding ->
@@ -55,6 +74,26 @@ fun OnboardingScreen(navController: NavController) {
                         .padding(horizontal = 32.dp)
                 ) {
                     navController.navigate(OnboardingActivity.Screen.Login.route.name)
+                }
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                if(composition == null) {
+                    Image(
+                        painter = painterResource(id = R.drawable.onboarding_image),
+                        contentDescription = "온보딩 이미지",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                } else {
+                    LottieAnimation(
+                        composition = composition,
+                        iterations = LottieConstants.IterateForever,
+                        speed = 0.1f,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
                 }
             }
         }
