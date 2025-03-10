962cdedd097fdd7e9ad77bbb3b7dd611c1884256
joonbo97 <joonbo97@naver.com>
2025-03-10 11:02:51 +0900

    refactor: error response 수정

--- app/src/main/java/com/store_me/storeme/utils/response/ResponseHandler.kt
+++ app/src/main/java/com/store_me/storeme/utils/response/ResponseHandler.kt
@@ -1,28 +1,28 @@
 package com.store_me.storeme.utils.response
 
 import android.content.Context
-import com.google.gson.Gson
 import com.store_me.storeme.R
-import com.store_me.storeme.data.response.StoreMeResponse
 import com.store_me.storeme.utils.exception.ApiExceptionHandler
+import org.json.JSONObject
 import retrofit2.Response
 import timber.log.Timber
 
 
 object ResponseHandler {
     fun <T> handleErrorResponse(response: Response<T>, context: Context): Result<Nothing> {
+        val statusCode = response.code()
         val errorBodyString = response.errorBody()?.string()
         Timber.w(errorBodyString)
 
-        return if (errorBodyString != null) {
-            val errorResponse = Gson().fromJson(errorBodyString, StoreMeResponse::class.java)
-            Result.failure(
-                ApiExceptionHandler.apiException(code = errorResponse.code, message = errorResponse.message)
-            )
-        } else {
-            Result.failure(
-                ApiExceptionHandler.apiException(code = null, message = context.getString(R.string.default_error_message))
-            )
+        val errorMessage = try {
+            val json = JSONObject(errorBodyString ?: "{}")
+            json.optString("message", context.getString(R.string.default_error_message))
+        } catch (e: Exception) {
+            context.getString(R.string.default_error_message)
         }
+
+        return Result.failure(
+            ApiExceptionHandler.apiException(code = statusCode, message = errorMessage)
+        )
     }
 }
