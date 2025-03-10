551bc8023a646c69d689340e0b71fcefd7e749ed
joonbo97 <joonbo97@naver.com>
2025-03-10 11:16:23 +0900

    feat: StoreInfoScreen 추가

---
+++ app/src/main/java/com/store_me/storeme/ui/store_info/StoreInfoScreen.kt
@@ -0,0 +1,13 @@
+package com.store_me.storeme.ui.store_info
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.store_me.storeme.ui.my_menu.MyMenuViewModel
+
+@Composable
+fun StoreInfoScreen(
+    navController: NavController
+) {
+
+}
