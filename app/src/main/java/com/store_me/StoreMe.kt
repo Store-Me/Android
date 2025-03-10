6e01b14afb909333a96cfaae69e17d4127106997
joonbo97 <joonbo97@naver.com>
2025-03-10 10:51:09 +0900

    feat: Firebase 초기화 추가

--- app/src/main/java/com/store_me/StoreMe.kt
+++ app/src/main/java/com/store_me/StoreMe.kt
@@ -1,6 +1,11 @@
 package com.store_me
 
 import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.appCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
 import com.kakao.sdk.common.KakaoSdk
 import com.store_me.storeme.BuildConfig
 import com.store_me.storeme.BuildConfig.KAKAO_KEY
@@ -17,8 +22,19 @@ class StoreMe : Application() {
         KakaoSdk.init(this, KAKAO_KEY)
         TokenPreferencesHelper.init(this)
 
+        FirebaseApp.initializeApp(this)
+
+
         if(BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+
+            Firebase.appCheck.installAppCheckProviderFactory(
+                DebugAppCheckProviderFactory.getInstance(),
+            )
+        } else {
+            Firebase.appCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance(),
+            )
         }
     }
 }
