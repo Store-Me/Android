e6b9e5f120d525370928c9745af44f1df8025f93
joonbo97 <joonbo97@naver.com>
2025-03-10 11:11:35 +0900

    feat: PhotoSettingScreen.kt 삭제

--- app/src/main/java/com/store_me/storeme/ui/store_setting/PhotoSettingScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.store_me.storeme.ui.store_setting
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun PhotoSettingScreen(navController: NavController) {
-}
