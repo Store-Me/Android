60ebb1937db3ac0f1ef63a276a2c4e4e9341af88
joonbo97 <joonbo97@naver.com>
2025-03-10 10:58:52 +0900

    feat: 버킷 이름을 관리하는 StoragePath 추가

---
+++ app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
@@ -0,0 +1,7 @@
+package com.store_me.storeme.utils
+
+object StoragePaths {
+    const val PROFILE_IMAGE = "profile_images"
+    const val STORE_PROFILE_IMAGE = "store_profile_images"
+    const val STORE_IMAGE = "store_images"
+}
