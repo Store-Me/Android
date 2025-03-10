49cb5e0cb09f2ea77f9c55d51782bb4a0afa111d
joonbo97 <joonbo97@naver.com>
2025-03-10 10:51:57 +0900

    feat: google service 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,6 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48.1")
+        classpath("com.google.gms:google-services:4.4.2")
     }
 }
