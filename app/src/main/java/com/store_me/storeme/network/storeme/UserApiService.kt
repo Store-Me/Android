678280725334bbe932c282ce676eb0a2c4c1e6ba
joonbo97 <joonbo97@naver.com>
2025-03-10 11:10:27 +0900

    feat: 사용자 관련 API 변경

--- app/src/main/java/com/store_me/storeme/network/storeme/UserApiService.kt
+++ app/src/main/java/com/store_me/storeme/network/storeme/UserApiService.kt
@@ -1,29 +1,36 @@
 package com.store_me.storeme.network.storeme
 
-import com.store_me.storeme.data.model.check_duplicate.CheckAccountIdDuplicate
 import com.store_me.storeme.data.response.CheckAccountIdDuplicateResponse
 import com.store_me.storeme.data.response.StoreMeResponse
-import com.store_me.storeme.data.request.AppLoginRequest
-import com.store_me.storeme.data.request.KakaoLoginRequest
+import com.store_me.storeme.data.request.login.AppLoginRequest
+import com.store_me.storeme.data.request.login.KakaoLoginRequest
 import com.store_me.storeme.data.response.LoginResponse
 import com.store_me.storeme.data.model.signup.CustomerSignupApp
 import com.store_me.storeme.data.model.signup.CustomerSignupKakao
+import com.store_me.storeme.data.model.signup.CustomerSignupRequest
 import com.store_me.storeme.data.model.signup.OwnerSignupApp
 import com.store_me.storeme.data.model.signup.OwnerSignupKakao
 import com.store_me.storeme.data.model.verification.ConfirmCode
 import com.store_me.storeme.data.model.verification.PhoneNumber
 import com.store_me.storeme.data.model.verification.PhoneNumberResponse
+import com.store_me.storeme.data.request.login.LoginRequest
 import okhttp3.MultipartBody
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Query
 
 interface UserApiService {
-    @Multipart
-    @POST("signup/app/customer")
+    @POST("signupCustomer")
+    suspend fun customerSignup(
+        @Body customerSignupRequest: CustomerSignupRequest
+    ): Response<Unit>
+
+    @POST("signupCustomer")
     suspend fun customerSignupApp(
         @Part profileImageFile: MultipartBody.Part?,
         @Part ("appCustomerSignupRequestDto") appCustomerSignupRequestDto: CustomerSignupApp
@@ -53,6 +60,11 @@ interface UserApiService {
         @Part("kakaoOwnerSignupRequestDto") kakaoOwnerSignupRequestDto: OwnerSignupKakao
     ): Response<StoreMeResponse<Unit>>
 
+    @POST("login")
+    suspend fun login(
+        @Body loginRequest: LoginRequest
+    ): Response<LoginResponse>
+
     @POST("login/app")
     suspend fun loginWithApp(
         @Body appLoginRequest: AppLoginRequest
@@ -73,10 +85,10 @@ interface UserApiService {
         @Body confirmCode: ConfirmCode
     ): Response<StoreMeResponse<Unit>>
 
-    @POST("signup/check/accountId")
+    @GET("checkAccountIdDuplicate")
     suspend fun checkAccountIdDuplication(
-        @Body accountId: CheckAccountIdDuplicate
-    ): Response<StoreMeResponse<CheckAccountIdDuplicateResponse>>
+        @Query("accountId") accountId: String
+    ): Response<CheckAccountIdDuplicateResponse>
 
     @DELETE("user")
     suspend fun deleteUser(
