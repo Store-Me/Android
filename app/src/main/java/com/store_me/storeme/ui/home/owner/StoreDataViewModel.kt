9773f9c1735e560206f266ff551e1fcfc3c4b315
joonbo97 <joonbo97@naver.com>
2025-03-19 23:02:46 +0900

    feat: Link 설정 추가

--- app/src/main/java/com/store_me/storeme/ui/home/owner/StoreDataViewModel.kt
+++ app/src/main/java/com/store_me/storeme/ui/home/owner/StoreDataViewModel.kt
@@ -1,111 +1,161 @@
 package com.store_me.storeme.ui.home.owner
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.store_me.auth.Auth
-import com.store_me.storeme.R
-import com.store_me.storeme.data.MenuCategoryData
-import com.store_me.storeme.data.StoreData
 import com.store_me.storeme.data.StoreHomeItem
+import com.store_me.storeme.data.request.store.PatchLinksRequest
+import com.store_me.storeme.data.request.store.PatchStoreProfileImagesRequest
+import com.store_me.storeme.data.store.BusinessHourData
+import com.store_me.storeme.data.store.StoreInfoData
 import com.store_me.storeme.repository.storeme.OwnerRepository
+import com.store_me.storeme.utils.ErrorEventBus
+import com.store_me.storeme.utils.SuccessEventBus
 import com.store_me.storeme.utils.exception.ApiException
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class StoreDataViewModel @Inject constructor(
-    @ApplicationContext private val context: Context,
-    private val auth: Auth,
     private val ownerRepository: OwnerRepository
 ): ViewModel() {
-    private val _storeData = MutableStateFlow<StoreData?>(null)
-    val storeData: StateFlow<StoreData?> = _storeData
+    private val _storeInfoData = MutableStateFlow<StoreInfoData?>(null)
+    val storeInfoData: StateFlow<StoreInfoData?> = _storeInfoData
 
-    val storeId = auth.storeId
+    private val _businessHours = MutableStateFlow<List<BusinessHourData>?>(emptyList())
+    val businessHours: StateFlow<List<BusinessHourData>?> = _businessHours
 
-    private val _menuCategoryList = MutableStateFlow<List<MenuCategoryData>>(emptyList())
-    val menuCategoryList: StateFlow<List<MenuCategoryData>> = _menuCategoryList
-
-    //오류 메시지
-    private val _errorMessage = MutableStateFlow<String?>(null)
-    val errorMessage: StateFlow<String?> = _errorMessage
-
-    init {
-        storeId.value?.let {
-            getStoreData(it)
-        }
-    }
+    private val _links = MutableStateFlow<List<String>?>(emptyList())
+    val links: StateFlow<List<String>?> = _links
 
     /**
      * StoreData 조회 함수
      */
     fun getStoreData(storeId: String) {
-        /*viewModelScope.launch {
+        viewModelScope.launch {
             val response = ownerRepository.getStoreData(storeId = storeId)
 
             response.onSuccess {
-                updateStoreData(it)
-
-                getMenuCategory(storeId)
+                updateStoreInfoData(it)
             }.onFailure {
                 if(it is ApiException) {
-                    _errorMessage.value = it.message
+                    ErrorEventBus.errorFlow.emit(it.message)
                 } else {
-                    _errorMessage.value = context.getString(R.string.unknown_error_message)
-                }
-            }
-        }*/
-    }
-
-    /**
-     * StoreData 갱신 함수
-     */
-    fun updateStoreData(storeData: StoreData) {
-        _storeData.value = storeData
-    }
-
-    /**
-     * MenuCategory 조회 함수
-     */
-    fun getMenuCategory(storeId: Long) {
-        viewModelScope.launch {
-            val response = ownerRepository.getMenuCategory(storeId = storeId)
-
-            response.onSuccess {
-                updateMenuCategoryList(it.storeMenuCategoryInfoList)
-            }.onFailure {
-                if(it is ApiException) {
-                    _errorMessage.value = it.message
-                } else {
-                    _errorMessage.value = context.getString(R.string.unknown_error_message)
+                    ErrorEventBus.errorFlow.emit(null)
                 }
             }
         }
     }
 
-    fun updateMenuCategoryList(menuCategoryList: List<MenuCategoryData>) {
-        _menuCategoryList.value = menuCategoryList
+    /**
+     * BusinessHours 조회 함수
+     */
+    fun getStoreBusinessHours(storeId:String) {
+        viewModelScope.launch {
+            val response = ownerRepository.getBusinessHours(storeId = storeId)
+
+            response.onSuccess {
+                updateBusinessHours(it.businessHours)
+            }.onFailure {
+                if(it is ApiException) {
+                    ErrorEventBus.errorFlow.emit(it.message)
+                } else {
+                    ErrorEventBus.errorFlow.emit(null)
+                }
+            }
+        }
     }
 
-    //Store Phone Number Copy 함수
-    fun copyToClipboard(storePhoneNumber: String){
-        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip = ClipData.newPlainText(storeData.value?.storeName, storePhoneNumber)
-        clipboard.setPrimaryClip(clip)
+    /**
+     * StoreLinks 조회 함수
+     */
+    fun getStoreLinks(storeId: String) {
+        viewModelScope.launch {
+            val response = ownerRepository.getStoreLinks(storeId = storeId)
+
+            response.onSuccess {
+                updateStoreLinks(it.links)
+            }.onFailure {
+                if(it is ApiException) {
+                    ErrorEventBus.errorFlow.emit(it.message)
+                } else {
+                    ErrorEventBus.errorFlow.emit(null)
+                }
+            }
+        }
+    }
+
+    fun patchStoreProfileImages(storeId: String, profileImage: String?, backgroundImage: String?) {
+        viewModelScope.launch {
+            val response = ownerRepository.patchStoreProfileImages(
+                storeId = storeId,
+                patchStoreProfileImagesRequest = PatchStoreProfileImagesRequest(storeProfileImage = profileImage, backgroundImage = backgroundImage)
+            )
+
+            response.onSuccess {
+                updateStoreInfoData(it.result)
+
+                SuccessEventBus.successFlow.emit(it.message)
+            }.onFailure {
+                if(it is ApiException) {
+                    ErrorEventBus.errorFlow.emit(it.message)
+                } else {
+                    ErrorEventBus.errorFlow.emit(null)
+                }
+            }
+        }
+    }
+
+    fun patchStoreLinks(storeId: String, links: List<String>) {
+        viewModelScope.launch {
+            val response = ownerRepository.patchStoreLinks(
+                storeId = storeId,
+                patchLinksRequest = PatchLinksRequest(links = links)
+            )
+
+            response.onSuccess {
+                updateStoreLinks(it.result.links)
+
+                SuccessEventBus.successFlow.emit(it.message)
+            }.onFailure {
+                if (it is ApiException) {
+                    ErrorEventBus.errorFlow.emit(it.message)
+                } else {
+                    ErrorEventBus.errorFlow.emit(null)
+                }
+            }
+        }
+    }
+
+    /**
+     * StoreData 갱신 함수
+     */
+    fun updateStoreInfoData(storeInfoData: StoreInfoData) {
+        _storeInfoData.value = storeInfoData
+    }
+
+    /**
+     * BusinessHours 갱신 함수
+     */
+    fun updateBusinessHours(businessHours: List<BusinessHourData>?) {
+        _businessHours.value = businessHours
+    }
+
+    /**
+     * StoreLinks 갱신 함수
+     */
+    fun updateStoreLinks(links: List<String>?) {
+        _links.value = links
     }
 
     //편집 버튼 Text
     fun getEditButtonText(storeHomeItem: StoreHomeItem, isEmpty: Boolean = true): String {
         return when(storeHomeItem) {
             StoreHomeItem.NOTICE -> { if (isEmpty) "공지사항 작성" else "공지사항 편집"}
-            StoreHomeItem.INTRO -> { if (isEmpty) "소개 내용 작성" else "소개 내용 편집"}
+            //StoreHomeItem.INTRO -> { if (isEmpty) "소개 내용 작성" else "소개 내용 편집"}
             StoreHomeItem.IMAGE -> { "사진 업로드"}
             StoreHomeItem.COUPON -> { "쿠폰 관리" }
             StoreHomeItem.MENU -> { "메뉴 관리" }
