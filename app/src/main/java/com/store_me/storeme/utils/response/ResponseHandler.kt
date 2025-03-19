4799d24d4e73c1c92be092a442cedd8b10ee6709
joonbo97 <joonbo97@naver.com>
2025-03-19 23:05:23 +0900

    refactor: context 제거

--- app/src/main/java/com/store_me/storeme/utils/response/ResponseHandler.kt
+++ app/src/main/java/com/store_me/storeme/utils/response/ResponseHandler.kt
@@ -1,7 +1,5 @@
 package com.store_me.storeme.utils.response
 
-import android.content.Context
-import com.store_me.storeme.R
 import com.store_me.storeme.utils.exception.ApiExceptionHandler
 import org.json.JSONObject
 import retrofit2.Response
@@ -9,16 +7,16 @@ import timber.log.Timber
 
 
 object ResponseHandler {
-    fun <T> handleErrorResponse(response: Response<T>, context: Context): Result<Nothing> {
+    fun <T> handleErrorResponse(response: Response<T>): Result<Nothing> {
         val statusCode = response.code()
         val errorBodyString = response.errorBody()?.string()
         Timber.w(errorBodyString)
 
         val errorMessage = try {
             val json = JSONObject(errorBodyString ?: "{}")
-            json.optString("message", context.getString(R.string.default_error_message))
+            json.optString("message", "서버와의 통신 중 오류가 발생했습니다. 다시 시도해 주세요.")
         } catch (e: Exception) {
-            context.getString(R.string.default_error_message)
+            "서버와의 통신 중 오류가 발생했습니다. 다시 시도해 주세요."
         }
 
         return Result.failure(
