ee6afe789e7b73bb5847931c32a8d6833e2ba281
joonbo97 <joonbo97@naver.com>
2025-03-19 23:11:57 +0900

    feat: Store 관련 API 추가

--- app/src/main/java/com/store_me/storeme/network/storeme/OwnerApiService.kt
+++ app/src/main/java/com/store_me/storeme/network/storeme/OwnerApiService.kt
@@ -1,106 +1,96 @@
 package com.store_me.storeme.network.storeme
 
-import com.store_me.storeme.data.MenuCategoryData
-import com.store_me.storeme.data.StoreData
-import com.store_me.storeme.data.request.menu.MenuCategoryRequest
-import com.store_me.storeme.data.request.menu.UpdateStoreMenuCategoryNameRequestDto
-import com.store_me.storeme.data.request.store_image.StoreImageOrderRequest
-import com.store_me.storeme.data.response.MenuCategoryList
-import com.store_me.storeme.data.response.StoreMeResponse
-import com.store_me.storeme.data.response.StoreListResponse
-import okhttp3.MultipartBody
+import com.store_me.storeme.data.request.store.PatchBusinessHoursRequest
+import com.store_me.storeme.data.request.store.PatchLinksRequest
+import com.store_me.storeme.data.request.store.PatchStoreDescriptionRequest
+import com.store_me.storeme.data.request.store.PatchStoreIntroRequest
+import com.store_me.storeme.data.request.store.PatchStoreProfileImagesRequest
+import com.store_me.storeme.data.response.BusinessHoursResponse
+import com.store_me.storeme.data.response.LinksResponse
+import com.store_me.storeme.data.response.MyStoresResponse
+import com.store_me.storeme.data.response.PatchResponse
+import com.store_me.storeme.data.store.StoreInfoData
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
-import retrofit2.http.Multipart
 import retrofit2.http.PATCH
-import retrofit2.http.POST
-import retrofit2.http.Part
 import retrofit2.http.Path
 
 interface OwnerApiService {
     /**
      * 가게 목록 요청 API
      */
-    @GET("store/list")
-    suspend fun getStoreList(
-
-    ): Response<StoreMeResponse<StoreListResponse>>
+    @GET("auth/stores")
+    suspend fun getMyStores(
+    ): Response<MyStoresResponse>
 
     /**
      * 가게 정보 요청 API
      */
-    @GET("store/{storeId}")
+    @GET("auth/store/{storeId}")
     suspend fun getStoreData(
-        @Path("storeId") storeId: Long
-    ): Response<StoreMeResponse<StoreData>>
+        @Path("storeId") storeId: String
+    ): Response<StoreInfoData>
 
     /**
-     * 가게 이미지 저장 API
+     * 가게 이미지 수정 API
      */
-    @Multipart
-    @POST("store/image/{storeId}")
-    suspend fun addStoreImages(
-        @Path("storeId") storeId: Long,
-        @Part storeImageFileList: List<MultipartBody.Part>
-    ): Response<StoreMeResponse<Unit>>
+    @PATCH("auth/store/{storeId}/images")
+    suspend fun patchStoreProfileImages(
+        @Path("storeId") storeId: String,
+        @Body patchStoreProfileImagesRequest: PatchStoreProfileImagesRequest
+    ): Response<PatchResponse<StoreInfoData>>
 
     /**
-     * 가게 이미지 순서 변경 API
+     * 가게 소개 수정 API
      */
-    @PATCH("store/image/order")
-    suspend fun patchStoreImageOrder(
-        @Body storeImageOrderRequest: StoreImageOrderRequest
-    ): Response<StoreMeResponse<Unit>>
+    @PATCH("auth/store/{storeId}/intro")
+    suspend fun patchStoreIntro(
+        @Path("storeId") storeId: String,
+        @Body patchStoreIntroRequest: PatchStoreIntroRequest
+    ): Response<PatchResponse<StoreInfoData>>
 
     /**
-     * 가게 이미지 삭제 API
+     * 가게 설명 수정 API
      */
-    @DELETE("store/image/{storeId}/{storeImageId}")
-    suspend fun deleteStoreImage(
-        @Path("storeId") storeId: Long,
-        @Path("storeImageId") storeImageId: Long
-    ): Response<StoreMeResponse<Unit>>
+    @PATCH("auth/store/{storeId}/description")
+    suspend fun patchStoreDescription(
+        @Path("storeId") storeId: String,
+        @Body patchStoreDescriptionRequest: PatchStoreDescriptionRequest
+    ): Response<PatchResponse<StoreInfoData>>
 
     /**
-     * 가게 메뉴 카테고리 조회
+     * 영업 시간 조회 API
      */
-    @GET("store/menu/category/{storeId}")
-    suspend fun getMenuCategory(
-        @Path("storeId") storeId: Long
-    ): Response<StoreMeResponse<MenuCategoryList>>
+    @GET("auth/store/{storeId}/business-hours")
+    suspend fun getBusinessHours(
+        @Path("storeId") storeId: String
+    ): Response<BusinessHoursResponse>
 
     /**
-     * 가게 메뉴 카테고리 추가
+     * 영업 시간 수정 API
      */
-    @POST("store/menu/category")
-    suspend fun addMenuCategory(
-        @Body saveStoreMenuCategoryRequestDto: MenuCategoryRequest
-    ): Response<StoreMeResponse<Unit>>
+    @PATCH("auth/store/{storeId}/business-hours")
+    suspend fun patchBusinessHours(
+        @Path("storeId") storeId: String,
+        @Body patchBusinessHoursRequest: PatchBusinessHoursRequest
+    ): Response<PatchResponse<BusinessHoursResponse>>
 
     /**
-     * 가게 메뉴 카테고리 순서 변경
+     * 가게 링크 조회 API
      */
-    @PATCH("store/menu/category/order")
-    suspend fun patchMenuCategoryOrder(
-        @Body updateStoreMenuCategoryOrderRequestDto: MenuCategoryList
-    ): Response<StoreMeResponse<Unit>>
+    @GET("auth/store/{storeId}/links")
+    suspend fun getStoreLinks(
+        @Path("storeId") storeId: String
+    ): Response<LinksResponse>
 
     /**
-     * 가게 메뉴 카테고리 이름 변경
+     * 가게 링크 수정 API
      */
-    @PATCH("store/menu/category/name")
-    suspend fun updateStoreMenuCategoryName(
-        @Body updateStoreMenuCategoryNameRequestDto: UpdateStoreMenuCategoryNameRequestDto
-    ): Response<StoreMeResponse<Unit>>
+    @PATCH("auth/store/{storeId}/links")
+    suspend fun patchStoreLinks(
+        @Path("storeId") storeId: String,
+        @Body patchLinksRequest: PatchLinksRequest
+    ): Response<PatchResponse<LinksResponse>>
 
-    /**
-     * 가게 메뉴 카테고리 삭제
-     */
-    @DELETE("store/menu/category/{storeId}/{storeMenuCategoryId}")
-    suspend fun deleteMenuCategory(
-        @Path("storeId") storeId: Long,
-        @Path("storeMenuCategoryId") storeMenuCategoryId: Int
-    ): Response<StoreMeResponse<Unit>>
 }
