baf49390dba18426f895563e0fdd486d4b90ce8d
joonbo97 <joonbo97@naver.com>
2025-03-19 23:04:53 +0900

    feat: store background path 추가

--- app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
+++ app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
@@ -3,5 +3,6 @@ package com.store_me.storeme.utils
 object StoragePaths {
     const val PROFILE_IMAGE = "profile_images"
     const val STORE_PROFILE_IMAGE = "store_profile_images"
+    const val STORE_BACKGROUND_IMAGE = "store_background_images"
     const val STORE_IMAGE = "store_images"
 }
