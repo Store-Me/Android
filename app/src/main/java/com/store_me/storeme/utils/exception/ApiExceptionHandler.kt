b4e5439a2a07255d41ec916034641978e15a28c9
joonbo97 <joonbo97@naver.com>
2025-03-19 22:11:33 +0900

    refactor: context를 사용하지 않도록 변경

--- app/src/main/java/com/store_me/storeme/utils/exception/ApiExceptionHandler.kt
+++ app/src/main/java/com/store_me/storeme/utils/exception/ApiExceptionHandler.kt
@@ -1,8 +1,6 @@
 package com.store_me.storeme.utils.exception
 
-import android.content.Context
 import com.google.gson.JsonSyntaxException
-import com.store_me.storeme.R
 import timber.log.Timber
 import java.io.IOException
 
@@ -12,26 +10,26 @@ object ApiExceptionHandler {
         return ApiException(code, errorMessage)
     }
 
-    fun Exception.toResult(context: Context): Result<Nothing> {
+    fun Exception.toResult(): Result<Nothing> {
         Timber.e(this)
 
         return when (this) {
             is IOException -> Result.failure(
                 apiException(
                     code = null,
-                    message = context.getString(R.string.network_error_message)
+                    message = "데이터 또는 와이파이 연결이 원활하지 않습니다. 연결을 확인 후 다시 시도해 주세요."
                 )
             )
             is JsonSyntaxException -> Result.failure(
                 apiException(
                     code = null,
-                    message = context.getString(R.string.json_error_message)
+                    message = "서버가 예상하지 못한 응답을 보냈습니다. 관리자에게 문의하세요."
                 )
             )
             else -> Result.failure(
                 apiException(
                     code = null,
-                    message = context.getString(R.string.default_error_message)
+                    message = "서버와의 통신 중 오류가 발생했습니다. 다시 시도해 주세요."
                 )
             )
         }
