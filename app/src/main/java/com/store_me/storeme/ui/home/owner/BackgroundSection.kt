2359cf35c346acd8045647186444e57c18154eb2
joonbo97 <joonbo97@naver.com>
2025-03-19 22:16:04 +0900

    feat: Background showCanvas 파라미터 추가

--- app/src/main/java/com/store_me/storeme/ui/home/owner/BackgroundSection.kt
+++ app/src/main/java/com/store_me/storeme/ui/home/owner/BackgroundSection.kt
@@ -1,5 +1,6 @@
 package com.store_me.storeme.ui.home.owner
 
+import android.net.Uri
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
@@ -16,9 +17,9 @@ import coil.compose.AsyncImage
  * @param imageUrl 배경 이미지 url
  */
 @Composable
-fun BackgroundSection(imageUrl: String?) {
+fun BackgroundSection(modifier: Modifier = Modifier, imageUrl: String?, showCanvas: Boolean) {
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .aspectRatio(2f / 1f)
     ) {
@@ -32,8 +33,39 @@ fun BackgroundSection(imageUrl: String?) {
             )
         }
 
-        Canvas(modifier = Modifier.fillMaxSize()) {
-            drawRect(color = Color.Black.copy(alpha = 0.3f))
+        if(showCanvas) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawRect(color = Color.Black.copy(alpha = 0.3f))
+            }
+        }
+    }
+}
+
+/**
+ * 가게 배경 이미지 Composable
+ * @param imageUri 배경 이미지 uri
+ */
+@Composable
+fun BackgroundSection(modifier: Modifier = Modifier, imageUri: Uri?, showCanvas: Boolean) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(2f / 1f)
+    ) {
+        imageUri?.let {
+            AsyncImage(
+                model = imageUri,
+                contentDescription = "배경 이미지",
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+
+        if(showCanvas) {
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                drawRect(color = Color.Black.copy(alpha = 0.3f))
+            }
         }
     }
 }
