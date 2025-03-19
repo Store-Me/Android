1f2318990f6208cdcbae4d251e1620f215d8679a
joonbo97 <joonbo97@naver.com>
2025-03-19 23:12:51 +0900

    feat: Store 이미지 변경 추가

--- app/src/main/java/com/store_me/storeme/ui/store_setting/profile/ProfileSettingViewModel.kt
+++ app/src/main/java/com/store_me/storeme/ui/store_setting/profile/ProfileSettingViewModel.kt
@@ -1,28 +1,127 @@
 package com.store_me.storeme.ui.store_setting.profile
 
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.store_me.auth.Auth
+import com.store_me.storeme.repository.storeme.ImageRepository
+import com.store_me.storeme.utils.ErrorEventBus
+import com.store_me.storeme.utils.StoragePaths
+import com.store_me.storeme.utils.exception.ApiException
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ProfileSettingViewModel: ViewModel() {
-    private val _backgroundImage = MutableStateFlow<String?>(null)
-    val backgroundImage: StateFlow<String?> = _backgroundImage
+@HiltViewModel
+class ProfileSettingViewModel @Inject constructor(
+    private val imageRepository: ImageRepository
+) : ViewModel() {
+    private val _backgroundImageUrl = MutableStateFlow<String?>(null)
+    val backgroundImageUrl: StateFlow<String?> = _backgroundImageUrl
 
-    fun updateBackgroundImage(newImageUrl: String) {
-        _backgroundImage.value = newImageUrl
+    private val _backgroundImageUri = MutableStateFlow<Uri?>(null)
+    val backgroundImageUri: StateFlow<Uri?> = _backgroundImageUri
+
+    private val _profileImageUrl = MutableStateFlow<String?>(null)
+    val profileImageUrl: StateFlow<String?> = _profileImageUrl
+
+    private val _profileImageUri = MutableStateFlow<Uri?>(null)
+    val profileImageUri: StateFlow<Uri?> = _profileImageUri
+
+    //이미지 업로드 상태
+    private val _profileImageProgress = MutableStateFlow<Float>(0.0f)
+    val profileImageProgress: StateFlow<Float> = _profileImageProgress
+
+    private val _backgroundImageProgress = MutableStateFlow<Float>(0.0f)
+    val backgroundImageProgress: StateFlow<Float> = _backgroundImageProgress
+
+    fun updateBackgroundImageUri(backgroundImageUri: Uri?) {
+        _backgroundImageUri.value = backgroundImageUri
+
+        if(backgroundImageUri == null) {
+            _backgroundImageUrl.value = null
+        }
     }
 
-    private val _profileImage = MutableStateFlow<String?>(null)
-    val profileImage: StateFlow<String?> = _profileImage
+    fun updateProfileImageUri(profileImageUri: Uri?) {
+        _profileImageUri.value = profileImageUri
 
-    fun updateProfileImage(newImageUrl: String) {
-        _profileImage.value = newImageUrl
+        if(profileImageUri == null) {
+            _profileImageUrl.value = null
+        }
     }
 
-    private val _storeName = MutableStateFlow<String>("")
-    val storeName: StateFlow<String> = _storeName
+    fun updateProfileImageUrl(profileImageUrl: String?) {
+        _profileImageUrl.value = profileImageUrl
+    }
 
-    fun updateStoreName(storeName: String) {
-        _storeName.value = storeName
+    fun updateBackgroundImageUrl(backgroundImageUrl: String?) {
+        _backgroundImageUrl.value = backgroundImageUrl
+    }
+
+    fun clearProfileProgress() {
+        _profileImageProgress.value = 0.0f
+        _profileImageUri.value = null
+    }
+
+    fun clearBackgroundProgress() {
+        _backgroundImageProgress.value = 0.0f
+        _backgroundImageUri.value = null
+    }
+
+    fun uploadStoreProfileImage(storeName: String) {
+        if(profileImageUri.value == null)
+            return
+
+        viewModelScope.launch {
+            val response = imageRepository.uploadImage(
+                folderName = StoragePaths.STORE_PROFILE_IMAGE,
+                uri = profileImageUri.value!!,
+                uniqueName = storeName
+            ) {
+                _profileImageProgress.value = it
+            }
+
+            response.onSuccess {
+                _profileImageUrl.value = it
+            }.onFailure {
+                updateProfileImageUri(null)
+
+                if(it is ApiException) {
+                    ErrorEventBus.errorFlow.emit(it.message)
+                } else {
+                    ErrorEventBus.errorFlow.emit(null)
+                }
+            }
+        }
+    }
+
+    fun uploadStoreBackgroundImage(storeName: String) {
+        if(backgroundImageUri.value == null)
+            return
+
+        viewModelScope.launch {
+            val response = imageRepository.uploadImage(
+                folderName = StoragePaths.STORE_BACKGROUND_IMAGE,
+                uri = backgroundImageUri.value!!,
+                uniqueName = storeName
+            ) {
+                _backgroundImageProgress.value = it
+            }
+
+            response.onSuccess {
+                _backgroundImageUrl.value = it
+            }.onFailure {
+                updateBackgroundImageUri(null)
+
+                if(it is ApiException) {
+                    ErrorEventBus.errorFlow.emit(it.message)
+                } else {
+                    ErrorEventBus.errorFlow.emit(null)
+                }
+            }
+        }
     }
 }
