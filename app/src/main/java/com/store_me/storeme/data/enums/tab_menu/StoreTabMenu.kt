dcf208e27e8c562b92caff799af16466fe7888a0
joonbo97 <joonbo97@naver.com>
2025-03-19 23:05:45 +0900

    feat: StoreTabMenu 추가

---
+++ app/src/main/java/com/store_me/storeme/data/enums/tab_menu/StoreTabMenu.kt
@@ -0,0 +1,11 @@
+package com.store_me.storeme.data.enums.tab_menu
+
+enum class StoreTabMenu(val displayName: String) {
+    HOME("스토어 홈"),
+    NEWS("소식"),
+    MENU("메뉴"),
+    COUPON("쿠폰"),
+    PHOTO("사진"),
+    STORY("스토리"),
+    REVIEW("후기"),
+}
