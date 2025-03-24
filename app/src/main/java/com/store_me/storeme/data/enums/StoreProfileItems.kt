63bea0fafa2ebbb60621db36ed76753f15aa9281
joonbo97 <joonbo97@naver.com>
2025-03-24 20:47:56 +0900

    feat: displayName 추가

--- app/src/main/java/com/store_me/storeme/data/enums/StoreProfileItems.kt
+++ app/src/main/java/com/store_me/storeme/data/enums/StoreProfileItems.kt
@@ -1,5 +1,5 @@
 package com.store_me.storeme.data.enums
 
-enum class StoreProfileItems {
-    INTRO, BUSINESS_HOURS, HOLIDAY, PHONE_NUMBER,LOCATION, EDIT_PROFILE, MANAGEMENT
+enum class StoreProfileItems(val displayName: String) {
+    INTRO("소개"), BUSINESS_HOURS("영업 시간"), HOLIDAY("휴무일"), PHONE_NUMBER("전화번호"), LOCATION("위치 "), EDIT_PROFILE("프로필 수정"), MANAGEMENT("가게정보 관리")
 }
