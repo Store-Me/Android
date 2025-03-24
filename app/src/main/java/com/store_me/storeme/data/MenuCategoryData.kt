010476c9846efa89664869d801a3a2ec3e027907
joonbo97 <joonbo97@naver.com>
2025-03-24 21:09:40 +0900

    chore: 사용하지 않는 데이터 삭제

--- app/src/main/java/com/store_me/storeme/data/MenuCategoryData.kt
+++
@@ -1,7 +0,0 @@
-package com.store_me.storeme.data
-
-data class MenuCategoryData(
-    val id: Long,
-    val category: String,
-    val order: Int
-)
--- app/src/main/java/com/store_me/storeme/data/response/MenuCategoryList.kt
+++
@@ -1,7 +0,0 @@
-package com.store_me.storeme.data.response
-
-import com.store_me.storeme.data.MenuCategoryData
-
-data class MenuCategoryList(
-    val storeMenuCategoryInfoList: List<MenuCategoryData>
-)
