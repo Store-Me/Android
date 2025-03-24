22d4ec1d411a28984effe7b1019198dc9b2d693c
joonbo97 <joonbo97@naver.com>
2025-03-24 20:44:50 +0900

    chore: 주석 추가

--- app/src/main/java/com/store_me/storeme/data/store/BusinessHourData.kt
+++ app/src/main/java/com/store_me/storeme/data/store/BusinessHourData.kt
@@ -1,5 +1,13 @@
 package com.store_me.storeme.data.store
 
+/**
+ * Business Hour Data Class
+ * @param openingTime 영업 시작 시간 (HH:mm)
+ * @param closingTime 영업 종료 시간 (HH:mm)
+ * @param startBreak 브레이크 타임 시작 시간 (HH:mm)
+ * @param endBreak 브레이크 타임 종료 시간 (HH:mm)
+ * @param isHoliday 휴무일 여부
+ */
 data class BusinessHourData(
     val openingTime: String?,
     val closingTime: String?,
