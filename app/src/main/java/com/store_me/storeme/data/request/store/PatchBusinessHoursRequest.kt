651245f08558da346d2d21b5ef8460f9fa4cd77b
joonbo97 <joonbo97@naver.com>
2025-03-24 21:01:31 +0900

    feat: extraInfo 추가

--- app/src/main/java/com/store_me/storeme/data/request/store/PatchBusinessHoursRequest.kt
+++ app/src/main/java/com/store_me/storeme/data/request/store/PatchBusinessHoursRequest.kt
@@ -3,5 +3,6 @@ package com.store_me.storeme.data.request.store
 import com.store_me.storeme.data.store.BusinessHourData
 
 data class PatchBusinessHoursRequest(
-    val businessHours: List<BusinessHourData>
+    val businessHours: List<BusinessHourData>,
+    val extraInfo: String? = null
 )
