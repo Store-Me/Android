b374fed74610136cec8e362524369d41a723b12b
joonbo97 <joonbo97@naver.com>
2025-03-25 17:28:03 +0900

    chore: 파일 이동

---
+++ app/src/main/java/com/store_me/storeme/ui/store_setting/location/LocationSettingScreen.kt
@@ -0,0 +1,13 @@
+package com.store_me.storeme.ui.store_setting.location
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+
+@Composable
+fun LocationSettingScreen(
+    navController: NavController,
+    locationSettingViewModel: LocationSettingViewModel = viewModel()
+) {
+
+}
