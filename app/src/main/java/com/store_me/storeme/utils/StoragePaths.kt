620af9104fc7a4ccd12f85038b9fb032aefacd1f
joonbo97 <joonbo97@naver.com>
2025-03-26 16:24:39 +0900

    chore: 오타 수정

--- app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
+++ app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
@@ -4,5 +4,5 @@ object StoragePaths {
     const val PROFILE_IMAGE = "profile_images"
     const val STORE_PROFILE_IMAGE = "store_profile_images"
     const val STORE_BACKGROUND_IMAGE = "store_background_images"
-    const val STORE_IMAGE = "store_images"
+    const val STORE_IMAGES = "store_images"
 }
