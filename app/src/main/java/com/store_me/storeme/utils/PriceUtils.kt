068b8417d8fc60c45802746ccd943bab6deea892
joonbo97 <joonbo97@naver.com>
2025-03-28 18:37:00 +0900

    feat: null 추가

--- app/src/main/java/com/store_me/storeme/utils/PriceUtils.kt
+++ app/src/main/java/com/store_me/storeme/utils/PriceUtils.kt
@@ -4,7 +4,10 @@ import java.text.NumberFormat
 import java.util.Locale
 
 class PriceUtils {
-    fun numberToPrice(price: Int): String {
+    fun numberToPrice(price: Int?): String {
+        if(price == null)
+            return ""
+
         val formatter = NumberFormat.getNumberInstance(Locale.US)
         return formatter.format(price) + "원"
     }
