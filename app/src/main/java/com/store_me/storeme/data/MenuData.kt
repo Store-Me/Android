4c33ee2394f4bf369bd91d970713abe643dbe328
joonbo97 <joonbo97@naver.com>
2025-03-28 18:40:10 +0900

    feat: 메뉴 데이터 수정

--- app/src/main/java/com/store_me/storeme/data/MenuData.kt
+++ app/src/main/java/com/store_me/storeme/data/MenuData.kt
@@ -1,43 +1,14 @@
 package com.store_me.storeme.data
 
-enum class MenuPriceType {
-    FIXED, RANGE, VARIABLE
-}
-data class MenuCategory(
-    val categoryName: String,
-    val menuList: List<MenuData>
-)
-
-//메뉴 카테고리에 특정 메뉴가 있는지 확인하는 확장함수
-fun MenuCategory.hasMenu(menuName: String): Int {
-    return menuList.indexOfFirst { it.name == menuName }
-}
-
-const val DEFAULT_MENU_CATEGORY = "기본 카테고리"
-
-/**
- * 메뉴 정보
- * @param name 메뉴 이름
- * @param price 가격
- * @param imageUrl 이미지 URL
- * @param description 설명
- * @param isSignature 대표 메뉴 여부
- * @param isPopular 인기메뉴 여부
- * @param isRecommend 사장님 추천 여부
- */
 data class MenuData(
     val name: String,
-    val price: MenuPrice,
-    val imageUrl: String,
-    val description: String,
+    val priceType: String,
+    val price: Int?,
+    val minPrice: Int?,
+    val maxPrice: Int?,
+    val image: String?,
+    val description: String?,
     val isSignature: Boolean,
     val isPopular: Boolean,
     val isRecommend: Boolean
 )
-
-sealed class MenuPrice {
-    data class Fixed(val price: Int): MenuPrice()
-    data class Range(val minPrice: Int?, val maxPrice: Int?): MenuPrice()
-
-    data object Variable: MenuPrice()
-}
