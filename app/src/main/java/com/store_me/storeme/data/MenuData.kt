a024599aabf7e779600499bd3e49a2ff608d6007
joonbo97 <joonbo97@naver.com>
2025-03-30 23:38:27 +0900

    feat: 가격 Int > Long 변경

--- app/src/main/java/com/store_me/storeme/data/MenuData.kt
+++ app/src/main/java/com/store_me/storeme/data/MenuData.kt
@@ -3,9 +3,9 @@ package com.store_me.storeme.data
 data class MenuData(
     val name: String,
     val priceType: String,
-    val price: Int?,
-    val minPrice: Int?,
-    val maxPrice: Int?,
+    val price: Long?,
+    val minPrice: Long?,
+    val maxPrice: Long?,
     val image: String?,
     val description: String?,
     val isSignature: Boolean,
