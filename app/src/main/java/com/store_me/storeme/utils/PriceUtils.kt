08649aa22470e65d483411db210493a3d108def7
joonbo97 <joonbo97@naver.com>
2025-03-30 23:36:51 +0900

    feat: 가격 텍스트 변경 추가

--- app/src/main/java/com/store_me/storeme/utils/PriceUtils.kt
+++ app/src/main/java/com/store_me/storeme/utils/PriceUtils.kt
@@ -1,14 +1,35 @@
 package com.store_me.storeme.utils
 
+import com.store_me.storeme.data.enums.menu.MenuPriceType
 import java.text.NumberFormat
 import java.util.Locale
 
 class PriceUtils {
-    fun numberToPrice(price: Int?): String {
-        if(price == null)
-            return ""
 
-        val formatter = NumberFormat.getNumberInstance(Locale.US)
-        return formatter.format(price) + "원"
+    fun numberToPrice(priceType: String, price: Long?, minPrice: Long?, maxPrice: Long?): String {
+        fun getPriceText(price: Long?): String {
+            if(price == null)
+                return ""
+
+            val formatter = NumberFormat.getNumberInstance(Locale.US)
+            return formatter.format(price) + "원"
+        }
+
+        return when(priceType) {
+            MenuPriceType.Fixed.name -> getPriceText(price)
+            MenuPriceType.Ranged.name -> {
+                val minPriceText = getPriceText(minPrice)
+                val maxPriceText = getPriceText(maxPrice)
+
+                when {
+                    minPrice != null && maxPrice != null -> "$minPriceText ~ $maxPriceText"
+                    minPrice == null && maxPrice != null -> "최대 $maxPriceText"
+                    minPrice != null && maxPrice == null -> "최소 $minPriceText"
+                    else -> { "" }
+                }
+            }
+            MenuPriceType.Variable.name -> "변동"
+            else -> { "" }
+        }
     }
 }
