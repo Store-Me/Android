d7a2c1e19c9aefd295e135cc42e6730542932d31
joonbo97 <joonbo97@naver.com>
2025-03-30 23:36:39 +0900

    feat: 이미지 폴더 추가

--- app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
+++ app/src/main/java/com/store_me/storeme/utils/StoragePaths.kt
@@ -5,4 +5,5 @@ object StoragePaths {
     const val STORE_PROFILE_IMAGE = "store_profile_images"
     const val STORE_BACKGROUND_IMAGE = "store_background_images"
     const val STORE_IMAGES = "store_images"
+    const val STORE_MENU_IMAGES = "store_menu_images"
 }
