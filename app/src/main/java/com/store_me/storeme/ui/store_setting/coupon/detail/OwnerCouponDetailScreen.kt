c3e5b6e152c6b2703fa5f8b24185600c88e325ea
joonbo97 <joonbo97@naver.com>
2025-04-03 05:18:51 +0900

    feat: 쿠폰 상세보기 UI 변경

--- app/src/main/java/com/store_me/storeme/ui/store_setting/coupon/detail/OwnerCouponDetailScreen.kt
+++ app/src/main/java/com/store_me/storeme/ui/store_setting/coupon/detail/OwnerCouponDetailScreen.kt
@@ -1,239 +1,275 @@
-@file:OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class,
-    ExperimentalSharedTransitionApi::class, ExperimentalFoundationApi::class
-)
+@file:OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class, ExperimentalFoundationApi::class)
 
 package com.store_me.storeme.ui.store_setting.coupon.detail
 
-import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.rememberPagerState
 import com.store_me.storeme.R
-import com.store_me.storeme.data.Auth
-import com.store_me.storeme.data.CouponQuantity
-import com.store_me.storeme.data.OwnerCouponDetailData
+import com.store_me.storeme.data.CouponData
 import com.store_me.storeme.data.UserReceivedCouponData
 import com.store_me.storeme.data.UserUsedCouponData
+import com.store_me.storeme.data.enums.coupon.CouponType
+import com.store_me.storeme.data.enums.tab_menu.CouponDetailTabMenu
 import com.store_me.storeme.ui.component.StoreMeTabContent
 import com.store_me.storeme.ui.component.StoreMeTabRow
-import com.store_me.storeme.ui.component.TitleWithDeleteButtonAtDetail
-import com.store_me.storeme.ui.store_setting.coupon.detail.OwnerCouponDetailViewModel.CouponDetailTabTitles
 import com.store_me.storeme.ui.store_setting.coupon.setting.CouponInfo
-import com.store_me.storeme.ui.theme.DefaultDividerColor
-import com.store_me.storeme.ui.theme.ExpiredTextColor
+import com.store_me.storeme.ui.theme.LightestHighlightColor
 import com.store_me.storeme.ui.theme.PostTimeTextColor
+import com.store_me.storeme.ui.theme.SubHighlightColor
 import com.store_me.storeme.ui.theme.storeMeTextStyle
 import com.store_me.storeme.utils.DateTimeUtils
 
-val LocalOwnerCouponDetailViewModel = staticCompositionLocalOf<OwnerCouponDetailViewModel> {
-    error("No LocalOwnerCouponDetailViewModel provided")
+@Composable
+fun OwnerCouponDetailScreen(
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    sharedTransitionScope: SharedTransitionScope,
+    ownerCouponDetailViewModel: OwnerCouponDetailViewModel,
+    onEdit: (String) -> Unit
+){
+    val pagerState = rememberPagerState()
+    val tabTitles = remember { CouponDetailTabMenu.getDisplayNames() }
+
+    val coupon by ownerCouponDetailViewModel.coupon.collectAsState()
+
+    //TODO 상세 정보 조회
+
+    if(coupon != null) {
+        LazyColumn {
+            item {
+                CouponDetailInfo(
+                    animatedVisibilityScope = animatedVisibilityScope,
+                    sharedTransitionScope = sharedTransitionScope,
+                    coupon = coupon!!,
+                    onEdit = { onEdit(coupon!!.type) },
+                    onAttach = {
+
+                    }
+                )
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+            }
+
+            stickyHeader { StoreMeTabRow(pagerState = pagerState, tabTitles = tabTitles) }
+
+            item {
+                StoreMeTabContent(tabTitles = tabTitles, pagerState = pagerState) {
+                    when(it) {
+                        CouponDetailTabMenu.Received.ordinal -> {
+                            //TODO 수령 사용자
+                        }
+                        CouponDetailTabMenu.Used.ordinal -> {
+                            //TODO 사용 사용자
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 @Composable
-fun OwnerCouponDetailScreen(
-    selectedCouponId: String,animatedVisibilityScope: AnimatedVisibilityScope, sharedTransitionScope: SharedTransitionScope,
-    ownerCouponDetailViewModel: OwnerCouponDetailViewModel = viewModel(),
-    onBack: () -> Unit
-){
-    val selectedCouponDetail = Auth.couponDetailList.value.find {
-        it.couponInfoData.couponId == selectedCouponId
-    }
+fun CouponDetailInfo(
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    sharedTransitionScope: SharedTransitionScope,
+    coupon: CouponData,
+    onEdit: () -> Unit,
+    onAttach: () -> Unit
+) {
+    var couponHeight by remember { mutableStateOf(0) }
 
-    val pagerState = rememberPagerState()
-    val tabTitles = enumValues<CouponDetailTabTitles>().map { it.displayName }
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .padding(top = 20.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        with(sharedTransitionScope){
+            CouponInfo(
+                coupon = coupon,
+                modifier = Modifier
+                    .weight(1f)
+                    .sharedElement(
+                        rememberSharedContentState(key = coupon.couponId),
+                        animatedVisibilityScope
+                    )
+                    .onGloballyPositioned {
+                        couponHeight = it.size.height
+                    })
+        }
 
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .height(with(LocalDensity.current) { (couponHeight).toDp() }),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            CouponDetailBox(
+                modifier = Modifier
+                    .weight(1f),
+                title = "쿠폰 생성일",
+                content = DateTimeUtils.formatToDate(coupon.createdAt)
+            )
 
-    BackHandler {
-        onBack()
-    }
+            CouponDetailBox(
+                modifier = Modifier
+                    .weight(1f),
+                title = "쿠폰 만료일",
+                content = DateTimeUtils.formatToDate(coupon.dueDate)
+            )
 
-    CompositionLocalProvider(LocalOwnerCouponDetailViewModel provides ownerCouponDetailViewModel) {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            containerColor = White,
-            topBar = { OwnerCouponDetailTopAppBar(scrollBehavior){ onBack() } },
-
-            content = { innerPadding ->
-                Column(
+            Row(
+                modifier = Modifier
+                    .weight(1f),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                CouponDetailBox(
                     modifier = Modifier
-                        .padding(innerPadding)
-                        .nestedScroll(scrollBehavior.nestedScrollConnection)
+                        .weight(1f),
+                    title = "발급된 쿠폰",
+                    content = 0.toString()  //TODO 값 가져오기
+                )
+
+                CouponDetailBox(
+                    modifier = Modifier
+                        .weight(1f),
+                    title = "사용된 쿠폰",
+                    content = 0.toString()
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .weight(1f),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                CouponDetailIconBox(
+                    modifier = Modifier
+                        .weight(1f),
+                    title = "수정하기",
+                    iconResource = R.drawable.ic_edit
                 ) {
+                    onEdit()
+                }
 
-                        if(selectedCouponDetail != null) {
-                            LazyColumn {
-                                item {
-                                    with(sharedTransitionScope){
-                                        CouponInfo(item = selectedCouponDetail, modifier = Modifier
-                                            .padding(horizontal = 20.dp)
-                                            .sharedElement(
-                                                rememberSharedContentState(key = selectedCouponDetail.couponInfoData.couponId),
-                                                animatedVisibilityScope
-                                            ))
-                                    }
-                                }
-
-                                item { Spacer(modifier = Modifier.height(20.dp)) }
-
-                                item { Text(
-                                    text = "( 쿠폰 생성일 ) ${DateTimeUtils().dateTimeToDateText(selectedCouponDetail.couponInfoData.createdAt)}",
-                                    style = storeMeTextStyle(FontWeight.Bold, -2),
-                                    color = ExpiredTextColor,
-                                    modifier = Modifier.padding(horizontal = 20.dp)
-                                ) }
-
-                                item { Spacer(modifier = Modifier.height(20.dp)) }
-
-                                item { HorizontalDivider(color = DefaultDividerColor, thickness = 1.dp) }
-
-                                item { Spacer(modifier = Modifier.height(20.dp)) }
-
-                                item { CouponCountSection(selectedCouponDetail) }
-
-                                item { Spacer(modifier = Modifier.height(20.dp)) }
-
-                                stickyHeader { StoreMeTabRow(pagerState = pagerState, tabTitles = tabTitles) }
-
-                                item {
-                                    StoreMeTabContent(tabTitles = tabTitles, state = pagerState) {
-                                        UserListForCoupon(CouponDetailTabTitles.entries[it])
-                                    }
-                                }
-                            }
-                        }
+                CouponDetailIconBox(
+                    modifier = Modifier
+                        .weight(1f),
+                    title = "소식에 첨부",
+                    iconResource = R.drawable.ic_attach
+                ) {
+                    onAttach()
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun CouponDetailBox(modifier: Modifier, title: String, content: String) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = LightestHighlightColor, shape = RoundedCornerShape(20))
+            .clip(shape = RoundedCornerShape(20)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = title,
+            style = storeMeTextStyle(FontWeight.ExtraBold, -1),
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(12.dp))
+
+        Text(
+            text = content,
+            style = storeMeTextStyle(FontWeight.ExtraBold, 0),
+            color = PostTimeTextColor
         )
     }
 }
 
 @Composable
-fun CouponCountSection(selectedCouponDetail: OwnerCouponDetailData) {
+fun CouponDetailIconBox(modifier: Modifier, title: String, iconResource: Int, onClick: () -> Unit) {
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 20.dp),
-        verticalArrangement = Arrangement.spacedBy(20.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = SubHighlightColor, shape = RoundedCornerShape(20))
+            .clip(shape = RoundedCornerShape(20))
+            .clickable { onClick() },
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        if(selectedCouponDetail.couponInfoData.quantity is CouponQuantity.Limit){
-            Row(
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Text(
-                    text = "발급한 쿠폰 수",
-                    style = storeMeTextStyle(FontWeight.ExtraBold, 2),
-                    modifier = Modifier.weight(1f)
-                )
+        Icon(
+            painter = painterResource(id = iconResource),
+            modifier = Modifier
+                .size(20.dp),
+            contentDescription = "쿠폰 생성일",
+            tint = Color.Black
+        )
 
-                Text(
-                    text = "${(selectedCouponDetail.couponInfoData.quantity as CouponQuantity.Limit).quantity}",
-                    style = storeMeTextStyle(FontWeight.ExtraBold, 2)
-                )
-            }
-        }
+        Spacer(modifier = Modifier.height(12.dp))
 
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = "받은 쿠폰 수",
-                style = storeMeTextStyle(FontWeight.ExtraBold, 2),
-                modifier = Modifier.weight(1f)
-            )
-
-            Text(
-                text = "${selectedCouponDetail.usedCouponData.receivedCount}",
-                style = storeMeTextStyle(FontWeight.ExtraBold, 2)
-            )
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(
-                text = "사용한 쿠폰 수",
-                style = storeMeTextStyle(FontWeight.ExtraBold, 2),
-                modifier = Modifier.weight(1f)
-            )
-
-            Text(
-                text = "${selectedCouponDetail.usedCouponData.usedCount}",
-                style = storeMeTextStyle(FontWeight.ExtraBold, 2)
-            )
-        }
-    }
-}
-
-@Composable
-fun UserListForCoupon(couponDetailTabTitles: CouponDetailTabTitles) {
-    val ownerCouponDetailViewModel = LocalOwnerCouponDetailViewModel.current
-
-    when(couponDetailTabTitles) {
-        CouponDetailTabTitles.RECEIVED -> {
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 20.dp),
-            ) {
-                ownerCouponDetailViewModel.sampleCouponWithUserData.userReceivedCouponList.forEach {
-                    ReceivedUserItem(item = it)
-                }
-            }
-        }
-
-        CouponDetailTabTitles.USED -> {
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 20.dp),
-                verticalArrangement = Arrangement.spacedBy(20.dp)
-            ) {
-                ownerCouponDetailViewModel.sampleCouponWithUserData.userUsedCouponList.forEach {
-                    UsedUserItem(item = it)
-                }
-            }
-        }
+        Text(
+            text = title,
+            style = storeMeTextStyle(FontWeight.ExtraBold, -1),
+            color = Color.Black
+        )
     }
 }
 
 @Composable
 fun ReceivedUserItem(item: UserReceivedCouponData) {
     Row(
-        modifier = Modifier.padding(vertical = 10.dp),
+        modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -259,7 +295,7 @@ fun ReceivedUserItem(item: UserReceivedCouponData) {
 @Composable
 fun UsedUserItem(item: UserUsedCouponData) {
     Row(
-        modifier = Modifier.padding(vertical = 10.dp),
+        modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -288,25 +324,11 @@ fun UsedUserItem(item: UserUsedCouponData) {
             Spacer(modifier = Modifier.height(10.dp))
 
             Text(
-                text = "( 사용 완료 ) ${DateTimeUtils().dateTimeToDateTimeText(item.usedAt)}",
+                text = "( 사용 완료 ) ${DateTimeUtils.dateTimeToDateTimeText(item.usedAt)}",
                 style = storeMeTextStyle(FontWeight.ExtraBold, 0),
                 color = PostTimeTextColor,
                 modifier = Modifier.fillMaxWidth()
             )
         }
     }
-}
-
-@Composable
-fun OwnerCouponDetailTopAppBar(scrollBehavior: TopAppBarScrollBehavior, onBack: () -> Unit) {
-    TopAppBar(
-        title = { TitleWithDeleteButtonAtDetail(title = "쿠폰 상세보기", isInTopAppBar = true){
-            onBack()
-        } },
-        scrollBehavior = scrollBehavior,
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = White,
-            scrolledContainerColor = White
-        )
-    )
 }
--- app/src/main/java/com/store_me/storeme/ui/store_setting/coupon/detail/OwnerCouponDetailViewModel.kt
+++ app/src/main/java/com/store_me/storeme/ui/store_setting/coupon/detail/OwnerCouponDetailViewModel.kt
@@ -1,52 +1,21 @@
 package com.store_me.storeme.ui.store_setting.coupon.detail
 
 import androidx.lifecycle.ViewModel
-import com.store_me.storeme.data.CouponWithUserData
-import com.store_me.storeme.data.UserData
-import com.store_me.storeme.data.UserReceivedCouponData
-import com.store_me.storeme.data.UserUsedCouponData
+import com.store_me.storeme.data.CouponData
+import com.store_me.storeme.repository.storeme.CustomerRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
-class OwnerCouponDetailViewModel: ViewModel() {
-    enum class CouponDetailTabTitles(val displayName: String){
-        RECEIVED("받은 손님"), USED("사용한 손님")
+@HiltViewModel
+class OwnerCouponDetailViewModel @Inject constructor(
+    private val customerRepository: CustomerRepository
+) : ViewModel() {
+    private val _coupon = MutableStateFlow<CouponData?>(null)
+    val coupon: StateFlow<CouponData?> = _coupon
+
+    fun updateCouponData(couponData: CouponData?) {
+        _coupon.value = couponData
     }
-
-    val sampleCouponWithUserData = CouponWithUserData(
-        couponId = "",
-        userReceivedCouponList = listOf(
-            UserReceivedCouponData(UserData("테스트 사용자1", "테스트 사용자1", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자2", "테스트 사용자2", "")),
-            UserReceivedCouponData(UserData("테스트 사용자3", "테스트 사용자3", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자4", "테스트 사용자4", "")),
-            UserReceivedCouponData(UserData("테스트 사용자5", "테스트 사용자5", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자6", "테스트 사용자6", "")),
-            UserReceivedCouponData(UserData("테스트 사용자7", "테스트 사용자7", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자8", "테스트 사용자8", "")),
-            UserReceivedCouponData(UserData("테스트 사용자9", "테스트 사용자9", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자10", "테스트 사용자10", "")),
-            UserReceivedCouponData(UserData("테스트 사용자11", "테스트 사용자11", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자12", "테스트 사용자12", "")),
-            UserReceivedCouponData(UserData("테스트 사용자13", "테스트 사용자13", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자14", "테스트 사용자14", "")),
-            UserReceivedCouponData(UserData("테스트 사용자15", "테스트 사용자15", "https://via.placeholder.com/200x200")),
-            UserReceivedCouponData(UserData("테스트 사용자16", "테스트 사용자16", "")),
-        ),
-        userUsedCouponList = listOf(
-            UserUsedCouponData(UserData("테스트 사용자1", "테스트 사용자1", "https://via.placeholder.com/200x200"), "2024-09-01T17:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자2", "테스트 사용자2", ""), "2024-09-01T18:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자3", "테스트 사용자3", "https://via.placeholder.com/200x200"), "2024-09-01T19:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자4", "테스트 사용자4", ""), "2024-09-01T20:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자5", "테스트 사용자5", "https://via.placeholder.com/200x200"), "2024-09-01T21:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자6", "테스트 사용자6", ""), "2024-09-01T22:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자7", "테스트 사용자7", "https://via.placeholder.com/200x200"), "2024-09-01T23:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자8", "테스트 사용자8", ""), "2024-09-02T17:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자9", "테스트 사용자9", "https://via.placeholder.com/200x200"), "2024-09-03T17:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자10", "테스트 사용자10", ""), "2024-09-04T17:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자11", "테스트 사용자11", "https://via.placeholder.com/200x200"), "2024-09-05T17:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자12", "테스트 사용자12", ""), "2024-09-06T07:25:00"),
-            UserUsedCouponData(UserData("테스트 사용자13", "테스트 사용자13", "https://via.placeholder.com/200x200"), "2024-09-06T07:26:00"),
-            UserUsedCouponData(UserData("테스트 사용자14", "테스트 사용자14", ""), "2024-09-06T07:27:00"),
-            UserUsedCouponData(UserData("테스트 사용자15", "테스트 사용자15", "https://via.placeholder.com/200x200"), "2024-09-06T07:28:00"),
-        )
-    )
 }
