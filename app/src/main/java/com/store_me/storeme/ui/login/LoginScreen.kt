4f2fad5fd024876e9a5fd2ee0c2fb01c5e54a3f4
joonbo97 <joonbo97@naver.com>
2025-04-03 05:11:50 +0900

    chore: padding 조정

--- app/src/main/java/com/store_me/storeme/ui/login/LoginScreen.kt
+++ app/src/main/java/com/store_me/storeme/ui/login/LoginScreen.kt
@@ -209,7 +209,7 @@ fun LoginScreen(
                 }
 
                 item {
-                    Spacer(modifier = Modifier.height(16.dp))
+                    Spacer(modifier = Modifier.height(8.dp))
                 }
 
                 item {
