ad9757f2bf19c6660c32bd60ebaa2c3cd760d944
joonbo97 <joonbo97@naver.com>
2025-04-03 05:19:39 +0900

    feat: 쿠폰 조회 API 추가

--- app/src/main/java/com/store_me/storeme/network/storeme/CustomerApiService.kt
+++ app/src/main/java/com/store_me/storeme/network/storeme/CustomerApiService.kt
@@ -1,8 +1,11 @@
 package com.store_me.storeme.network.storeme
 
+import com.store_me.storeme.data.response.CouponResponse
 import com.store_me.storeme.data.response.CustomerInfoResponse
+import com.store_me.storeme.data.response.PatchResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CustomerApiService {
     /**
@@ -12,4 +15,12 @@ interface CustomerApiService {
     suspend fun getCustomerInfo(
 
     ): Response<CustomerInfoResponse>
+
+    /**
+     * 쿠폰 Id를 통해 단일 쿠폰을 조회
+     */
+    @GET("auth/coupon/{couponId}")
+    suspend fun getCouponData(
+        @Path("couponId") couponId: String
+    ): Response<PatchResponse<CouponResponse>>
 }
--- app/src/main/java/com/store_me/storeme/repository/storeme/CustomerRepository.kt
+++ app/src/main/java/com/store_me/storeme/repository/storeme/CustomerRepository.kt
@@ -1,11 +1,18 @@
 package com.store_me.storeme.repository.storeme
 
+import com.store_me.storeme.data.response.CouponResponse
 import com.store_me.storeme.data.response.CustomerInfoResponse
+import com.store_me.storeme.data.response.PatchResponse
 import com.store_me.storeme.network.storeme.CustomerApiService
+import com.store_me.storeme.utils.exception.ApiExceptionHandler.toResult
+import com.store_me.storeme.utils.response.ResponseHandler
+import timber.log.Timber
 import javax.inject.Inject
 
 interface CustomerRepository {
     suspend fun getCustomerInfo(): Result<CustomerInfoResponse>
+
+    suspend fun getCouponData(couponId: String): Result<PatchResponse<CouponResponse>>
 }
 
 class CustomerRepositoryImpl @Inject constructor(
@@ -13,5 +20,29 @@ class CustomerRepositoryImpl @Inject constructor(
 ): CustomerRepository {
     override suspend fun getCustomerInfo(): Result<CustomerInfoResponse> {
         return Result.success(customerApiService.getCustomerInfo().body()!!)
+    }
+
+    override suspend fun getCouponData(couponId: String): Result<PatchResponse<CouponResponse>> {
+        return try {
+            val response = customerApiService.getCouponData(
+                couponId = couponId
+            )
+
+            if(response.isSuccessful) {
+                val responseBody = response.body()
+
+                Timber.d(responseBody.toString())
+
+                if(responseBody != null) {
+                    Result.success(responseBody)
+                } else {
+                    ResponseHandler.handleErrorResponse(response)
+                }
+            } else {
+                ResponseHandler.handleErrorResponse(response)
+            }
+        } catch (e: Exception) {
+            e.toResult()
+        }
     }
 }
