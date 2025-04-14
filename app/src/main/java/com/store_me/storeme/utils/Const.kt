8cd91dae5ec4fb81aa3bfcc0ef098cbf77e31321
joonbo97 <joonbo97@naver.com>
2025-04-15 01:32:18 +0900

    feat: dp로 수정

--- app/src/main/java/com/store_me/storeme/utils/Const.kt
+++ app/src/main/java/com/store_me/storeme/utils/Const.kt
@@ -1,8 +1,10 @@
 package com.store_me.storeme.utils
 
+import androidx.compose.ui.unit.dp
+
 const val DEFAULT_MENU_CATEGORY_NAME = "기본 카테고리"
 
 /**
  * 가장 밖의 Box Rounding 값
  */
-const val BACKGROUND_ROUNDING_VALUE = 12
+val BACKGROUND_ROUNDING_VALUE = 12.dp
