f629dde69908a28352440986442241584d92c26f
joonbo97 <joonbo97@naver.com>
2025-04-30 12:16:38 +0900

    chore: 주석 추가

--- app/src/main/java/com/store_me/storeme/data/store/post/LabelData.kt
+++ app/src/main/java/com/store_me/storeme/data/store/post/LabelData.kt
@@ -1,5 +1,12 @@
 package com.store_me.storeme.data.store.post
 
+/**
+ * data class for label data
+ * @param labelId label id
+ * @param name label name
+ * @param order label order
+ * @param postCount label post count
+ */
 data class LabelData(
     val labelId: String,
     val name: String,
