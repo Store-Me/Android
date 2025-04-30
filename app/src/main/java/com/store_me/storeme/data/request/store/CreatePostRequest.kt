806c3da1168d1158f72761252843731baf683558
joonbo97 <joonbo97@naver.com>
2025-04-30 12:16:33 +0900

    chore: 주석 추가

--- app/src/main/java/com/store_me/storeme/data/request/store/CreatePostRequest.kt
+++ app/src/main/java/com/store_me/storeme/data/request/store/CreatePostRequest.kt
@@ -1,5 +1,10 @@
 package com.store_me.storeme.data.request.store
 
+/**
+ * data class for request to create a normal post
+ * @param type type of content
+ * @param content content of the post
+ */
 data class CreatePostRequest(
     val type: String,
     val labelId: String,
@@ -7,6 +12,11 @@ data class CreatePostRequest(
     val content: List<ContentData>
 )
 
+/**
+ * data class for content data
+ * @param type type of content
+ * @param content content of the content
+ */
 data class ContentData(
     val type: String,
     val content: String
