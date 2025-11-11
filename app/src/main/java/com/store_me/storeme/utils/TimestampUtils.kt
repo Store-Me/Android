d94003f871d1a96c12cf5e82cf37b424632f9e65
joonbo97 <joonbo97@naver.com>
2025-11-11 10:45:11 +0900

    feat: 시간 경과 표시 기능 개선

--- app/src/main/java/com/store_me/storeme/utils/TimestampUtils.kt
+++ app/src/main/java/com/store_me/storeme/utils/TimestampUtils.kt
@@ -4,6 +4,9 @@ import com.google.firebase.Timestamp
 import java.time.Duration
 import java.time.Instant
 
+/**
+ * 시간 경과를 출력하는 확장함수
+ */
 fun Timestamp.toTimeAgo(): String {
     val now = Instant.now()
     val timestampInstant = this.toDate().toInstant()
@@ -14,8 +17,8 @@ fun Timestamp.toTimeAgo(): String {
         duration.toMinutes() < 60 -> "${duration.toMinutes()}분 전"
         duration.toHours() < 24 -> "${duration.toHours()}시간 전"
         duration.toDays() < 7 -> "${duration.toDays()}일 전"
-        else -> {
-            ""
-        }
+        duration.toDays() < 30 -> "${duration.toDays() / 7}주 전"
+        duration.toDays() < 365 -> "${duration.toDays() / 30}개월 전"
+        else -> "${duration.toDays() / 365}년 전"
     }
 }
