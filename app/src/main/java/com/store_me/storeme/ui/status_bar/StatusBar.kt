d7d2c445ab2dbd62a2b29d1059f4662d241612c9
joonbo97 <joonbo97@naver.com>
2025-11-11 10:44:28 +0900

    feat: StatusBar Padding Composable 추가

--- app/src/main/java/com/store_me/storeme/ui/status_bar/StatusBar.kt
+++ app/src/main/java/com/store_me/storeme/ui/status_bar/StatusBar.kt
@@ -1,14 +1,20 @@
 package com.store_me.storeme.ui.status_bar
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 /**
  * 반투명한 Status Bar를 위한 Composable
@@ -16,7 +22,7 @@ import androidx.compose.ui.platform.LocalDensity
  */
 @Composable
 fun StatusBarProtection(
-    heightProvider: () -> Float = calculateGradientHeight(),
+    heightProvider: () -> Float = calculateStatusBarHeightPixel(),
 ) {
 
     Canvas(Modifier.fillMaxSize()) {
@@ -29,9 +35,33 @@ fun StatusBarProtection(
     }
 }
 
+/**
+ * StatusBar 만큼의 Padding을 제공하는 Composable
+ */
 @Composable
-fun calculateGradientHeight(): () -> Float {
+fun StatusBarPadding(
+    heightProvider: () -> Dp = calculateStausBarHeightDp()
+) {
+    val calculatedHeight = heightProvider()
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(calculatedHeight)
+    )
+}
+
+@Composable
+fun calculateStatusBarHeightPixel(): () -> Float {
     val statusBars = WindowInsets.statusBars
     val density = LocalDensity.current
     return { statusBars.getTop(density).times(1f) }
+}
+
+@Composable
+fun calculateStausBarHeightDp(): () -> Dp {
+    val statusBars = WindowInsets.statusBars
+    val topPaddingDp = statusBars.asPaddingValues().calculateTopPadding()
+
+    return { topPaddingDp }
 }
